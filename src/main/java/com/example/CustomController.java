9785fa73d7c3cefea98b7dd5327082d15407510b
Onkar1642 <onkarP7620@gmail.com>
2025-01-29 17:32:56 +0530

    hii onkar

--- src/main/java/com/example/CustomController.java
+++ src/main/java/com/example/CustomController.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/help")
 public class CustomController {
+	
+	
+	
 
 }
